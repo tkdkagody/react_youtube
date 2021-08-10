97477fbf1628f04c7b85d47807a9c36216c95d53
tkdkagody <tkdkagody@gmail.com>
2021-08-11 03:44:48 +0900

    cleanup & responsible

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -6,10 +6,11 @@ const VideoDetail = ({ video, video: { snippet } }) => (
     <iframe 
       className={styles.video}
       type="text/html" 
+      title="youtube video player"
       width="100%" height="500"
       src={`https://www.youtube.com/embed/${video.id}`}
-      frameborder="0" 
-      allowfullscreen>
+      frameBorder="0" 
+      allowFullscreen>
     </iframe>
     <h2>{snippet.title}</h2>
     <h3>{snippet.channelTitle}</h3>
