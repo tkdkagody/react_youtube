3b9e691d5874715c8565f24ff349602a5be767d0
tkdkagody <tkdkagody@gmail.com>
2021-08-11 01:04:06 +0900

    refac 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,32 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/serach_header';
 import VideoList from './components/video_list/video_list'
 
 
 
-function App() {
+function App( {youtube} ) {
 
   const [videos, setVideos] = useState([]);
 
 
+  const search = query => {
+    youtube.search(query)//
+    .then(videos => setVideos(videos))
+  };
+
+
   useEffect(()=> {
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'  //  옵션
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCmMTOqH4nK04LI2eU09jHdnN1s2e_1h3I", requestOptions)
-      .then(res =>  res.json())  //정상으로 받으면 텍스트로 변환
-      .then(result => setVideos(result.items))  //변환 텍스트를 출력 
-      .catch(err => console.log('err', err));
-
-
+    youtube.mostPopular()//
+    .then(videos => setVideos(videos))
+   
   },[])
 
   return (
-   <>
+   <div className={styles.app}>
+   <SearchHeader onSearch={search}/>
    <VideoList videos={videos} />
-   </>
+   </div>
   );
 }
 
